5914e6a67725d2d8e913ec8d7f25be820bd59087
Evilnood <evilnood666@gmail.com>
2021-04-04 14:38:15 +0300

    Added template in class

--- Classes.cpp
+++ Classes.cpp
@@ -1,6 +1,6 @@
 ﻿
 #include <iostream>
-
+//using namespace std;
 class Rectangle //creating class
 {
 private:
@@ -54,6 +54,39 @@ public:
     }
 };
 
+template <class temp>   //creating template
+
+class Other_Rectangle
+{
+private:
+    temp width;                //radius in any template (int or float for example)
+    temp height;
+public:
+    Other_Rectangle();
+    Other_Rectangle(temp x, temp y);
+   
+    temp area()
+    {
+        temp r;
+        r = width * height;
+        return r;
+    }
+};
+
+template <class temp>       //necessary
+Other_Rectangle<temp>::Other_Rectangle()
+{
+    width = 1;
+    height = 1;
+}
+
+template <class temp>       //necessary
+Other_Rectangle<temp>::Other_Rectangle(temp x, temp y)
+{
+    width = x;
+    height = y;
+}
+
 int main()
 {
     int x, y;
@@ -83,4 +116,11 @@ int main()
     sum_of_classes = rectangle_1 + rectangle_2;
 
     std::cout << "Area of sum of classes: " << sum_of_classes.area() << std::endl;  
+
+    Other_Rectangle<int> circle_1(5, 4);
+    Other_Rectangle<double> circle_2(3.6, 7.2);
+
+    std::cout << "Area of circle 1: " << circle_1.area() << std::endl;
+    std::cout << "Area of circle 2: " << circle_2.area() << std::endl;
+
 }
